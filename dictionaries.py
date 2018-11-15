650da5c8af9745155e82947646c9ae6c75011535
seanmcquaid <sean.mcquaid2@verizon.net>
2018-11-15 11:14:50 -0500

    Initial commit

---
+++ dictionaries.py
@@ -0,0 +1,13 @@
+# a dictionary is like a list of variables
+
+# key : value pairs 
+
+greg = {
+    "name" : "greg",
+    "gender" : "male",
+    "height" : "Tall",
+    "Job" : "developer"
+}
+
+print greg["name"]
+print greg["gender"]
